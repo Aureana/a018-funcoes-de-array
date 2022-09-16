a7d297fbd265b92ea474fd789bbd2654f319d82c
Aureana <111321782+Aureana@users.noreply.github.com>
2022-09-15 22:55:46 -0300

    Atividade funções de array resolvidas a018

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,3 +1,8 @@
+//# Exercício 3
+
+//Dado um array de produtos, onde cada produto é um objeto com nome, preço e categoria, 
+//retorne um novo array com o nome dos produtos da categoria Limpeza.
+
 const produtos = [
     { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
     { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
@@ -10,4 +15,16 @@ const produtos = [
     { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
     { nome: "Sabão em Pó", categoria: "Limpeza", preco: 10.80 }
  ]
+ //let array = []
+ const produtosLimpeza = produtos.filter ((produtos) => {
+      return produtos.categoria === "Limpeza" 
+ })
  
+ 
+// map((produto)=>{
+//     return produto.nome
+//  })
+
+  console.log ( produtosLimpeza )
+
+
